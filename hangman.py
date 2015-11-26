c0c0b7b82c74302d74c4537fb66151307eedc182
Anna Nylander <Anna.chiakichama@gmail.com>
2015-11-26 21:59:13 +0100

    basic architecture of the program

---
+++ hangman.py
@@ -0,0 +1,62 @@
+
+# Variables
+lives = 6
+word = "undefined"
+guessedLetters = ""
+
+#The main loop of the program
+def main():
+    return;
+
+#The game loop (one game)
+def game():
+    return;
+
+#Reset variables
+def restartGame():
+    return;
+
+#----------------------------------------------
+#GAME LOGIC
+
+# Check if lives left
+def alive():
+    return lives <= 0;
+
+# Check if player has won
+def won():
+    return Bool;
+
+#Player makes a new guess 
+def guess(letters):
+    # in loop:
+        #If lives left check next letter in string.
+        #Check if won
+    return;
+
+#Remove already guessed letter from the new guess.
+def stripGuess (letters):
+    return newWord;
+
+#Check if letter guessed is correct
+def checkLetter (letter):
+    return Bool;
+
+#------------------------------------------------
+#COMMANDLINE IO
+
+def getGuess():
+    str = raw_input("Enter your guess: ")
+    return;
+
+
+#-----------------------------------------------
+#FILE IO
+
+def getNewWord():
+    file dictionary = open(file_name, "r")
+    #check number of lines in doc
+
+    #get line on random line
+    dictionary.close();
+    return word;
